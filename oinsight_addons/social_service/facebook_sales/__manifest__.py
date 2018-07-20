afb0c93a4fbd16b5e9511e51ce87184cbff5c1d2
nguyendat.5997@gmail.com <nguyendat.5997@gmail.com>
2018-07-20 16:40:21 +0700

    Facebook

--- oinsight_addons/social_service/facebook_sales/__manifest__.py
+++ oinsight_addons/social_service/facebook_sales/__manifest__.py
@@ -25,8 +25,9 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        # 'views/views.xml',
+        # 'views/templates.xml',
+        'views/facebook_view.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
